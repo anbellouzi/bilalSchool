63981b08cd4da9cfe05b8a7aaacaa1a9809f9050
Anas <anbellouzi@gmail.com>
2020-06-03 14:46:01 -0700

    deployment

--- bilalSchool/settings.py
+++ bilalSchool/settings.py
@@ -52,9 +52,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bilalSchool.urls'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
